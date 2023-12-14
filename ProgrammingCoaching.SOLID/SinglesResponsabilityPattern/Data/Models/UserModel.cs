9071d732eddb4798507f014c680607e122db1473
scherenhaenden <scherenhaenden@gmail.com>
2023-12-14 15:07:45 +0100

    remove bad position files

--- ProgrammingCoaching.SOLID/SinglesResponsabilityPattern/Data/Models/UserModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ProgrammingCoaching.SOLID.SinglesResponsabilityPattern.Data.Models;
-
-public class UserModel
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Password { get; set; } = null!;
-}
